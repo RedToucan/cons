16ef120555c8b7a41d9fc4af967c89c77ab036a6
RedToucan <kskeptic@gmail.com>
2026-07-06 23:39:05 +0900

    feat: add post about Oedipus myth and ecology of dispersal under culture/ecology

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,6 +37,7 @@ const subcategoryMap: { [key: string]: string } = {
   mcconnell: "미치 매코널",
   machinepolitics: "기계정치",
   chomsky: "노암 촘스키",
+  ecology: "생태학",
 };
 
 export default async function Home({ searchParams }: Props) {
